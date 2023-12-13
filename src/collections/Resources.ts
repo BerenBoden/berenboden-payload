8e92a311d701a38007fa89a81d33dfc37a3656be
berenboden <beren.boden@gmail.com>
2023-12-13 17:21:43 +1300

    initial commit

---
+++ src/collections/Resources.ts
@@ -0,0 +1,13 @@
+import { CollectionConfig } from "payload/types";
+
+const Resources: CollectionConfig = {
+  slug: "resources",
+  fields: [
+    {
+      name: "myField",
+      type: "text",
+    },
+  ],
+};
+
+export default Resources;
