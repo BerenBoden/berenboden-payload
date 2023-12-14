cda890fa584969cde8523d50b6e8fa18adb4a65e
berenboden <beren.boden@gmail.com>
2023-12-14 20:58:43 +1300

    added Resource

--- src/collections/Resources.ts
+++ src/collections/Resources.ts
@@ -2,10 +2,68 @@ import { CollectionConfig } from "payload/types";
 
 const Resources: CollectionConfig = {
   slug: "resources",
+  access: {
+    read: () => true, // Public read access
+    create: ({ req: { user } }) => Boolean(user), // Only authenticated users
+    update: ({ req: { user } }) => Boolean(user), // Only authenticated users
+    delete: ({ req: { user } }) => Boolean(user), // Only authenticated users
+  },
   fields: [
     {
-      name: "myField",
+      name: "title",
       type: "text",
+    },
+    {
+      name: "cover",
+      type: "text",
+    },
+    {
+      name: "coverAltText",
+      type: "text",
+    },
+    {
+      name: "featured",
+      type: "checkbox",
+    },
+    {
+      name: "content",
+      type: "richText",
+    },
+    {
+      name: "resource", // required
+      type: "radio", // required
+      options: [
+        // required
+        {
+          label: "article",
+          value: "article",
+        },
+        {
+          label: "certification",
+          value: "certification",
+        },
+        {
+          label: "project",
+          value: "project",
+        },
+        {
+          label: "technology",
+          value: "technology",
+        },
+      ],
+      admin: {
+        layout: "horizontal",
+      },
+    },
+    {
+      name: "externalLinks",
+      type: "array",
+      fields: [
+        {
+          name: "externalLink",
+          type: "text",
+        },
+      ],
     },
   ],
 };
